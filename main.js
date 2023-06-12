90786891326b3ef8895a48a19e1a99cb3b27a5ff
Jack <jackwebdev31@gmail.com>
2023-06-12 12:42:03 +0200

    making changes to the FAQs

--- main.js
+++ main.js
@@ -36,6 +36,20 @@ hamburger.addEventListener('click', () => {
 })
 
 
+function toggleAnswer(id) {
+  var answer = document.getElementById("answer" + id);
+  var toggleIcon = document.querySelector(".faq:nth-child(" + (id + 1) + ") .toggle-icon");
+
+  if (answer.style.display === "none") {
+    answer.style.display = "block";
+    toggleIcon.textContent = "-";
+  } else {
+    answer.style.display = "none";
+    toggleIcon.textContent = "+";
+  }
+}
+
+
 
 
 
