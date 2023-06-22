1c3e4e169669cbf65010b60308f94cfa30a4ca0f
Kunal Virk <kunal@websilt.com>
2023-06-23 00:26:02 +0530

    Minor changes in navbar styles and JS (#1)
    * Update navbar.js
    
    Add an active class to the current page
    
    * Update navbar.css
    
    Style for active class

--- navbar.js
+++ navbar.js
@@ -7,6 +7,22 @@ const loadNavbar = async () => {
     const body = await response.text();
     navbarContent.innerHTML = body;
 
+    // Add `active` class
+    const getPage = window.location.pathname.split('/')[1];
+    console.log("d", getPage);
+
+    var menuItems = document.querySelectorAll('nav .nav__menu li a');
+
+    if (getPage === "" || getPage === "index.html") {
+      menuItems[0].classList.add("active");
+    } else {
+      for (var i = 0, len = menuItems.length; i < len; i++) {
+          if (menuItems[i].getAttribute("href").indexOf(getPage) !== -1) {
+              menuItems[i].classList.add("active");
+          }
+      }
+    }
+
     const navMenu = document.querySelector('#navMenu');
     const hamburger = document.querySelector('#hamburger');
 
